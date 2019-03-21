32b354a7f7ce07e5d8f905e523d00f0d4d51d212
Andrew James <ajames20@gmail.com>
2019-03-21 17:26:22 -0500

    Add more tests and export an object

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-module.exports.sample = () => {
-  console.log('cool beans')
-  return 'cool beans'
-}
+const sample = () => {
+  console.log('cool beans');
+  return 'cool beans';
+};
 
 // const addButton = document.querySelector('add-idea');
 
 // addButton.addEventListener('click', () => sample());
+
+const math = (numOne = 0, numTwo = 0) => {
+  return numOne + numTwo;
+};
+
+module.exports = {
+  sample,
+  math
+};
--- test/test-main.js
+++ test/test-main.js
@@ -1,12 +1,23 @@
-var main = require('../src/main');
-var expect = require('chai').expect;
+const main = require('../src/main');
+const {expect} = require('chai');
 
 describe('sample()', () => {
   context('this is to check things are working', () => {
     const expected = 'cool beans';
     it('should return the sample string', () => {
-      expect(main.sample()).to.be.a('string').that.includes(expected)
-    })
-  })
+      expect(main.sample()).to.be.a('string').that.includes(expected);
+    });
+  });
 
-})
+  context('this is to check more things are working', () => {
+    it('should return 0', () => {
+      const expected = 0;
+      expect(main.math()).to.be.a('number').that.equals(expected);
+    });
+
+    it('should return 2', () => {
+      const expected = 2;
+      expect(main.math(1, 1)).to.be.a('number').that.equals(expected);
+    });
+  });
+});
